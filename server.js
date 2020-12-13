bcc5b139f750369d6c627d5a35f441ad080c61d0
Pboerma <boerma.pieter@gmail.com>
2020-12-13 12:52:19 -0500

    added server.js code

---
+++ server.js
@@ -0,0 +1,112 @@
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+
+//set up the Express application
+//set up port to use
+var app = express();
+var PORT = process.env.PORT || 8000;
+
+
+//code for the express ppp to handle data parsing
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//ROUTES - make sure the input is connected to the HTML
+//Route that sends user to the AJAX page
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+  
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  
+  });
+  
+  app.post("/api/notes", function (req, res) {
+    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
+      if (error) {
+        return console.log(error)
+      }
+      notes = JSON.parse(notes)
+  
+      var id = notes[notes.length - 1].id + 1
+      var newNote = { title: req.body.title, text: req.body.text, id: id }
+      var activeNote = notes.concat(newNote)
+  
+      fs.writeFile(__dirname + "/db/db.json", JSON.stringify(activeNote), function (error, data) {
+        if (error) {
+          return error
+        }
+        console.log(activeNote)
+        res.json(activeNote);
+      })
+    })
+  })
+
+
+//Pull from db.json
+
+app.get("/api/notes", function (req, res) {
+    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, data) {
+      if (error) {
+        return console.log(error)
+      }
+      console.log("This is Notes", data)
+      res.json(JSON.parse(data))
+    })
+  });
+  
+  app.delete("/api/notes/:id", function (req, res) {
+    const noteId = JSON.parse(req.params.id)
+    console.log(noteId)
+    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
+      if (error) {
+        return console.log(error)
+      }
+      notes = JSON.parse(notes)
+  
+      notes = notes.filter(val => val.id !== noteId)
+  
+      fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function (error, data) {
+        if (error) {
+          return error
+        }
+        res.json(notes)
+      })
+    })
+  })
+
+  app.put("/api/notes/:id", function(req, res) {
+    const noteId = JSON.parse(req.params.id)
+    console.log(noteId)
+    fs.readFile(__dirname + "db/db.json", "utf8", function(error, notes) {
+      if (error ){
+        return console.log(error)
+      }
+      notes.JSONparse(notes)
+  
+      notes = notes.filter(val => val.id !== noteId)
+  
+      fs.writeFile(__dirname +"db/db.json", JSON.stringify(notes), function (error, data) {
+        if (error) {
+          return error
+        }
+        res.json(notes)
+      })
+    })
+  })
+  
+  // Starts the server to begin listening
+  // =============================================================
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
+  
+
+
+
+
